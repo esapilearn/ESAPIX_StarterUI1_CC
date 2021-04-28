07eba0d8d678befff846dc2ddcf24c46beab26aa
Corey Clift <corey.clift@gmail.com>
2021-04-28 10:21:40 -0700

    working view model vs 1.0 success

---
+++ ESAPIX_StarterUI1_CC/ViewModels/PlanConstraint.cs
@@ -0,0 +1,30 @@
+﻿using ESAPIX.Constraints;
+using Prism.Mvvm;
+
+namespace ESAPX_StarterUI.ViewModels
+{
+    public class PlanConstraint : BindableBase//bindable base helps update UI
+    {
+        public PlanConstraint(IConstraint con)
+        {
+            this.Constraint = con;
+        }
+
+
+        private IConstraint _constraint;
+
+        public IConstraint Constraint
+        {
+            get { return _constraint; }
+            set { SetProperty(ref _constraint, value); }//so UI knows to update
+        }
+
+        private ConstraintResult _result;
+
+        public ConstraintResult Result
+        {
+            get { return _result; }
+            set { SetProperty(ref _result, value); }
+        }
+    }
+}
